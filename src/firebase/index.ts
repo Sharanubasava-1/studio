e7eb6c249ed816d3d2edd51dd77efdf4f31a0860
sharanubasava aradhya <sharanuaradhya0511@gmail.com>
2025-11-20 13:11:00 +0000

    Fix the error

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,20 +5,21 @@ import { getFirestore, type Firestore } from 'firebase/firestore';
 
 // We are intentionally not exporting the config object from `config.ts`
 // It is a server-only file and should not be imported in client components.
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
 
 let app: FirebaseApp;
 let auth: Auth;
 let firestore: Firestore;
 
 function initializeFirebase() {
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
+  
   if (!getApps().length) {
     app = initializeApp(firebaseConfig);
     auth = getAuth(app);
@@ -33,5 +34,6 @@ function initializeFirebase() {
 
 export { initializeFirebase };
 
-export { FirebaseProvider, useFirebase, useFirebaseApp, useAuth, useFirestore, FirebaseClientProvider } from './provider';
+export { FirebaseProvider, useFirebase, useFirebaseApp, useAuth, useFirestore } from './provider';
+export { FirebaseClientProvider } from './client-provider';
 export { useUser } from './auth/use-user';
