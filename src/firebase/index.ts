4a3bdf22d01de1a46dcaa86b7922a0f4066fef61
sharanubasava aradhya <sharanuaradhya0511@gmail.com>
2025-11-20 13:13:51 +0000

    File changes

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,21 +5,13 @@ import { getFirestore, type Firestore } from 'firebase/firestore';
 
 // We are intentionally not exporting the config object from `config.ts`
 // It is a server-only file and should not be imported in client components.
+import { firebaseConfig } from './config';
 
 let app: FirebaseApp;
 let auth: Auth;
 let firestore: Firestore;
 
-function initializeFirebase() {
-  const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  };
-  
+function initializeFirebase() {  
   if (!getApps().length) {
     app = initializeApp(firebaseConfig);
     auth = getAuth(app);
